52abd352a655f0a7026916f4d8602fc91a417234
Hugo Locurcio <hugo.locurcio@hugo.pro>
2023-05-20 01:07:44 +0200

    Add `--color` command line argument to control colored console output
    This also adds support for the `NO_COLOR` environment variable,
    and harmonizes checks for colored console output by using a unique Engine
    method.
    
    - `auto` (default): Only color if output is a TTY (interactive session).
      Color is disabled if the `NO_COLOR` environment variable is set to a
      non-empty string, as per <https://no-color.org>.
    - `always`: Always color, even if output is a file or is non-interactive
      (useful for continuous integration, which is non-interactive).
      This also bypasses `NO_COLOR` as per its specification.
    - `never`: Never color command line output.

--- core/string/print_string.cpp
+++ core/string/print_string.cpp
@@ -95,19 +95,21 @@ void __print_line_rich(const String &p_string) {
 	// especially for italic and strikethrough.
 	String p_string_ansi = p_string;
 
-	p_string_ansi = p_string_ansi.replace("[b]", "\u001b[1m");
-	p_string_ansi = p_string_ansi.replace("[/b]", "\u001b[22m");
-	p_string_ansi = p_string_ansi.replace("[i]", "\u001b[3m");
-	p_string_ansi = p_string_ansi.replace("[/i]", "\u001b[23m");
-	p_string_ansi = p_string_ansi.replace("[u]", "\u001b[4m");
-	p_string_ansi = p_string_ansi.replace("[/u]", "\u001b[24m");
-	p_string_ansi = p_string_ansi.replace("[s]", "\u001b[9m");
-	p_string_ansi = p_string_ansi.replace("[/s]", "\u001b[29m");
+	const bool should_color = Engine::get_singleton()->is_coloring_standard_output();
+
+	p_string_ansi = p_string_ansi.replace("[b]", should_color ? "\u001b[1m" : "");
+	p_string_ansi = p_string_ansi.replace("[/b]", should_color ? "\u001b[22m" : "");
+	p_string_ansi = p_string_ansi.replace("[i]", should_color ? "\u001b[3m" : "");
+	p_string_ansi = p_string_ansi.replace("[/i]", should_color ? "\u001b[23m" : "");
+	p_string_ansi = p_string_ansi.replace("[u]", should_color ? "\u001b[4m" : "");
+	p_string_ansi = p_string_ansi.replace("[/u]", should_color ? "\u001b[24m" : "");
+	p_string_ansi = p_string_ansi.replace("[s]", should_color ? "\u001b[9m" : "");
+	p_string_ansi = p_string_ansi.replace("[/s]", should_color ? "\u001b[29m" : "");
 
 	p_string_ansi = p_string_ansi.replace("[indent]", "    ");
 	p_string_ansi = p_string_ansi.replace("[/indent]", "");
-	p_string_ansi = p_string_ansi.replace("[code]", "\u001b[2m");
-	p_string_ansi = p_string_ansi.replace("[/code]", "\u001b[22m");
+	p_string_ansi = p_string_ansi.replace("[code]", should_color ? "\u001b[2m" : "");
+	p_string_ansi = p_string_ansi.replace("[/code]", should_color ? "\u001b[22m" : "");
 	p_string_ansi = p_string_ansi.replace("[url]", "");
 	p_string_ansi = p_string_ansi.replace("[/url]", "");
 	p_string_ansi = p_string_ansi.replace("[center]", "\n\t\t\t");
@@ -116,52 +118,52 @@ void __print_line_rich(const String &p_string) {
 	p_string_ansi = p_string_ansi.replace("[/right]", "");
 
 	if (p_string_ansi.contains("[color")) {
-		p_string_ansi = p_string_ansi.replace("[color=black]", "\u001b[30m");
-		p_string_ansi = p_string_ansi.replace("[color=red]", "\u001b[91m");
-		p_string_ansi = p_string_ansi.replace("[color=green]", "\u001b[92m");
-		p_string_ansi = p_string_ansi.replace("[color=lime]", "\u001b[92m");
-		p_string_ansi = p_string_ansi.replace("[color=yellow]", "\u001b[93m");
-		p_string_ansi = p_string_ansi.replace("[color=blue]", "\u001b[94m");
-		p_string_ansi = p_string_ansi.replace("[color=magenta]", "\u001b[95m");
-		p_string_ansi = p_string_ansi.replace("[color=pink]", "\u001b[38;5;218m");
-		p_string_ansi = p_string_ansi.replace("[color=purple]", "\u001b[38;5;98m");
-		p_string_ansi = p_string_ansi.replace("[color=cyan]", "\u001b[96m");
-		p_string_ansi = p_string_ansi.replace("[color=white]", "\u001b[97m");
-		p_string_ansi = p_string_ansi.replace("[color=orange]", "\u001b[38;5;208m");
-		p_string_ansi = p_string_ansi.replace("[color=gray]", "\u001b[90m");
-		p_string_ansi = p_string_ansi.replace("[/color]", "\u001b[39m");
+		p_string_ansi = p_string_ansi.replace("[color=black]", should_color ? "\u001b[30m" : "");
+		p_string_ansi = p_string_ansi.replace("[color=red]", should_color ? "\u001b[91m" : "");
+		p_string_ansi = p_string_ansi.replace("[color=green]", should_color ? "\u001b[92m" : "");
+		p_string_ansi = p_string_ansi.replace("[color=lime]", should_color ? "\u001b[92m" : "");
+		p_string_ansi = p_string_ansi.replace("[color=yellow]", should_color ? "\u001b[93m" : "");
+		p_string_ansi = p_string_ansi.replace("[color=blue]", should_color ? "\u001b[94m" : "");
+		p_string_ansi = p_string_ansi.replace("[color=magenta]", should_color ? "\u001b[95m" : "");
+		p_string_ansi = p_string_ansi.replace("[color=pink]", should_color ? "\u001b[38;5;218m" : "");
+		p_string_ansi = p_string_ansi.replace("[color=purple]", should_color ? "\u001b[38;5;98m" : "");
+		p_string_ansi = p_string_ansi.replace("[color=cyan]", should_color ? "\u001b[96m" : "");
+		p_string_ansi = p_string_ansi.replace("[color=white]", should_color ? "\u001b[97m" : "");
+		p_string_ansi = p_string_ansi.replace("[color=orange]", should_color ? "\u001b[38;5;208m" : "");
+		p_string_ansi = p_string_ansi.replace("[color=gray]", should_color ? "\u001b[90m" : "");
+		p_string_ansi = p_string_ansi.replace("[/color]", should_color ? "\u001b[39m" : "");
 	}
 	if (p_string_ansi.contains("[bgcolor")) {
-		p_string_ansi = p_string_ansi.replace("[bgcolor=black]", "\u001b[40m");
-		p_string_ansi = p_string_ansi.replace("[bgcolor=red]", "\u001b[101m");
-		p_string_ansi = p_string_ansi.replace("[bgcolor=green]", "\u001b[102m");
-		p_string_ansi = p_string_ansi.replace("[bgcolor=lime]", "\u001b[102m");
-		p_string_ansi = p_string_ansi.replace("[bgcolor=yellow]", "\u001b[103m");
-		p_string_ansi = p_string_ansi.replace("[bgcolor=blue]", "\u001b[104m");
-		p_string_ansi = p_string_ansi.replace("[bgcolor=magenta]", "\u001b[105m");
-		p_string_ansi = p_string_ansi.replace("[bgcolor=pink]", "\u001b[48;5;218m");
-		p_string_ansi = p_string_ansi.replace("[bgcolor=purple]", "\u001b[48;5;98m");
-		p_string_ansi = p_string_ansi.replace("[bgcolor=cyan]", "\u001b[106m");
-		p_string_ansi = p_string_ansi.replace("[bgcolor=white]", "\u001b[107m");
-		p_string_ansi = p_string_ansi.replace("[bgcolor=orange]", "\u001b[48;5;208m");
-		p_string_ansi = p_string_ansi.replace("[bgcolor=gray]", "\u001b[100m");
-		p_string_ansi = p_string_ansi.replace("[/bgcolor]", "\u001b[49m");
+		p_string_ansi = p_string_ansi.replace("[bgcolor=black]", should_color ? "\u001b[40m" : "");
+		p_string_ansi = p_string_ansi.replace("[bgcolor=red]", should_color ? "\u001b[101m" : "");
+		p_string_ansi = p_string_ansi.replace("[bgcolor=green]", should_color ? "\u001b[102m" : "");
+		p_string_ansi = p_string_ansi.replace("[bgcolor=lime]", should_color ? "\u001b[102m" : "");
+		p_string_ansi = p_string_ansi.replace("[bgcolor=yellow]", should_color ? "\u001b[103m" : "");
+		p_string_ansi = p_string_ansi.replace("[bgcolor=blue]", should_color ? "\u001b[104m" : "");
+		p_string_ansi = p_string_ansi.replace("[bgcolor=magenta]", should_color ? "\u001b[105m" : "");
+		p_string_ansi = p_string_ansi.replace("[bgcolor=pink]", should_color ? "\u001b[48;5;218m" : "");
+		p_string_ansi = p_string_ansi.replace("[bgcolor=purple]", should_color ? "\u001b[48;5;98m" : "");
+		p_string_ansi = p_string_ansi.replace("[bgcolor=cyan]", should_color ? "\u001b[106m" : "");
+		p_string_ansi = p_string_ansi.replace("[bgcolor=white]", should_color ? "\u001b[107m" : "");
+		p_string_ansi = p_string_ansi.replace("[bgcolor=orange]", should_color ? "\u001b[48;5;208m" : "");
+		p_string_ansi = p_string_ansi.replace("[bgcolor=gray]", should_color ? "\u001b[100m" : "");
+		p_string_ansi = p_string_ansi.replace("[/bgcolor]", should_color ? "\u001b[49m" : "");
 	}
 	if (p_string_ansi.contains("[fgcolor")) {
-		p_string_ansi = p_string_ansi.replace("[fgcolor=black]", "\u001b[30;40m");
-		p_string_ansi = p_string_ansi.replace("[fgcolor=red]", "\u001b[91;101m");
-		p_string_ansi = p_string_ansi.replace("[fgcolor=green]", "\u001b[92;102m");
-		p_string_ansi = p_string_ansi.replace("[fgcolor=lime]", "\u001b[92;102m");
-		p_string_ansi = p_string_ansi.replace("[fgcolor=yellow]", "\u001b[93;103m");
-		p_string_ansi = p_string_ansi.replace("[fgcolor=blue]", "\u001b[94;104m");
-		p_string_ansi = p_string_ansi.replace("[fgcolor=magenta]", "\u001b[95;105m");
-		p_string_ansi = p_string_ansi.replace("[fgcolor=pink]", "\u001b[38;5;218;48;5;218m");
-		p_string_ansi = p_string_ansi.replace("[fgcolor=purple]", "\u001b[38;5;98;48;5;98m");
-		p_string_ansi = p_string_ansi.replace("[fgcolor=cyan]", "\u001b[96;106m");
-		p_string_ansi = p_string_ansi.replace("[fgcolor=white]", "\u001b[97;107m");
-		p_string_ansi = p_string_ansi.replace("[fgcolor=orange]", "\u001b[38;5;208;48;5;208m");
-		p_string_ansi = p_string_ansi.replace("[fgcolor=gray]", "\u001b[90;100m");
-		p_string_ansi = p_string_ansi.replace("[/fgcolor]", "\u001b[39;49m");
+		p_string_ansi = p_string_ansi.replace("[fgcolor=black]", should_color ? "\u001b[30;40m" : "");
+		p_string_ansi = p_string_ansi.replace("[fgcolor=red]", should_color ? "\u001b[91;101m" : "");
+		p_string_ansi = p_string_ansi.replace("[fgcolor=green]", should_color ? "\u001b[92;102m" : "");
+		p_string_ansi = p_string_ansi.replace("[fgcolor=lime]", should_color ? "\u001b[92;102m" : "");
+		p_string_ansi = p_string_ansi.replace("[fgcolor=yellow]", should_color ? "\u001b[93;103m" : "");
+		p_string_ansi = p_string_ansi.replace("[fgcolor=blue]", should_color ? "\u001b[94;104m" : "");
+		p_string_ansi = p_string_ansi.replace("[fgcolor=magenta]", should_color ? "\u001b[95;105m" : "");
+		p_string_ansi = p_string_ansi.replace("[fgcolor=pink]", should_color ? "\u001b[38;5;218;48;5;218m" : "");
+		p_string_ansi = p_string_ansi.replace("[fgcolor=purple]", should_color ? "\u001b[38;5;98;48;5;98m" : "");
+		p_string_ansi = p_string_ansi.replace("[fgcolor=cyan]", should_color ? "\u001b[96;106m" : "");
+		p_string_ansi = p_string_ansi.replace("[fgcolor=white]", should_color ? "\u001b[97;107m" : "");
+		p_string_ansi = p_string_ansi.replace("[fgcolor=orange]", should_color ? "\u001b[38;5;208;48;5;208m" : "");
+		p_string_ansi = p_string_ansi.replace("[fgcolor=gray]", should_color ? "\u001b[90;100m" : "");
+		p_string_ansi = p_string_ansi.replace("[/fgcolor]", should_color ? "\u001b[39;49m" : "");
 	}
 
 	p_string_ansi += "\u001b[0m"; // Reset.
