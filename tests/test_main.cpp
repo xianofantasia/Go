d36c64f0c9141ef0a95e8042575c67a18da57961
DESKTOP-UT43QTQ\Garrigan-Desktop <matorin57@gmail.com>
2023-09-09 17:53:51 -0500

    Added getter tests for dynamic fonts

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -99,6 +99,7 @@
 #include "tests/scene/test_curve.h"
 #include "tests/scene/test_curve_2d.h"
 #include "tests/scene/test_curve_3d.h"
+#include "tests/scene/test_font.h"
 #include "tests/scene/test_gradient.h"
 #include "tests/scene/test_navigation_agent_2d.h"
 #include "tests/scene/test_navigation_agent_3d.h"
