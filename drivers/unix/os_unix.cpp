9ccaa20a1d4df02ece699d12dd1a21e2692324d2
2750558108 <119792950+L2750558108@users.noreply.github.com>
2024-02-05 12:34:27 +0800

    Update os_unix.cpp

--- drivers/unix/os_unix.cpp
+++ drivers/unix/os_unix.cpp
@@ -703,7 +703,8 @@ Error OS_Unix::set_cwd(const String &p_cwd) {
 String OS_Unix::get_cwd() const {
 	String cwd;
 	char ret[PATH_MAX];
-	ERR_FAIL_NULL_V(getcwd(ret, PATH_MAX), "");
+	char* temp = getcwd(ret,PATH_MAX);
+	ERR_FAIL_NULL_V(temp, "");
 	if (cwd.parse_utf8(ret) != OK) {
 		cwd = ret; // No utf8, maybe latin1?
 	}
