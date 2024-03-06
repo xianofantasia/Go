rgyani/godot

Improve export encryption robustness.
Add support for APK/AAB assets encryption using PCK encryption key/export config.
Enforce encrypted core files and file directory usage, and disable certain unsafe features to prevent external code/overrides loading.

core/config/project_settings.h
@@ -139,7 +139,7 @@ protected:
 
 	void _convert_to_last_version(int p_from_version);
 
-	bool _load_resource_pack(const String &p_pack, bool p_replace_files = true, int p_offset = 0);
+	bool _load_resource_pack(const String &p_pack, bool p_replace_files = true, int p_offset = 0, bool p_require_encrypion = false);
 
 	void _add_property_info_bind(const Dictionary &p_info);
 
@@ -149,6 +149,11 @@ protected:
 
 protected:
 	static void _bind_methods();
+
+#ifndef DISABLE_DEPRECATED
+	bool _load_resource_pack_76161(const String &p_pack, bool p_replace_files, int p_offset);
+	static void _bind_compatibility_methods();
+#endif
 
 public:
 	static const int CONFIG_VERSION = 5;

editor/export/project_export.cpp
@@ -357,13 +357,9 @@ void ProjectExportDialog::_edit_preset(int p_index) {
 	bool enc_pck_mode = current->get_enc_pck();
 	enc_pck->set_pressed(enc_pck_mode);
 
-	enc_directory->set_disabled(!enc_pck_mode);
 	enc_in_filters->set_editable(enc_pck_mode);
 	enc_ex_filters->set_editable(enc_pck_mode);
 	script_key->set_editable(enc_pck_mode);
-
-	bool enc_directory_mode = current->get_enc_directory();
-	enc_directory->set_pressed(enc_directory_mode);
 
 	String key = current->get_script_encryption_key();
 	if (!updating_script_key) {
@@ -537,23 +533,9 @@ void ProjectExportDialog::_enc_pck_changed(bool p_pressed) {
 	ERR_FAIL_COND(current.is_null());
 
 	current->set_enc_pck(p_pressed);
-	enc_directory->set_disabled(!p_pressed);
 	enc_in_filters->set_editable(p_pressed);
 	enc_ex_filters->set_editable(p_pressed);
 	script_key->set_editable(p_pressed);
-
-	_update_current_preset();
-}
-
-void ProjectExportDialog::_enc_directory_changed(bool p_pressed) {
-	if (updating) {
-		return;
-	}
-
-	Ref<EditorExportPreset> current = get_current_preset();
-	ERR_FAIL_COND(current.is_null());
-
-	current->set_enc_directory(p_pressed);
 
 	_update_current_preset();
 }
@@ -1304,13 +1286,8 @@ ProjectExportDialog::ProjectExportDialog() {
 
 	enc_pck = memnew(CheckButton);
 	enc_pck->connect("toggled", callable_mp(this, &ProjectExportDialog::_enc_pck_changed));
-	enc_pck->set_text(TTR("Encrypt Exported PCK"));
+	enc_pck->set_text(TTR("Encrypt Exported PCK or APK Assets"));
 	sec_vb->add_child(enc_pck);
-
-	enc_directory = memnew(CheckButton);
-	enc_directory->connect("toggled", callable_mp(this, &ProjectExportDialog::_enc_directory_changed));
-	enc_directory->set_text(TTR("Encrypt Index (File Names and Info)"));
-	sec_vb->add_child(enc_directory);
 
 	enc_in_filters = memnew(LineEdit);
 	enc_in_filters->connect("text_changed", callable_mp(this, &ProjectExportDialog::_enc_filters_changed));

platform/android/file_access_android.cpp
@@ -30,9 +30,96 @@
 
 #include "file_access_android.h"
 
+#include "core/object/script_language.h"
 #include "core/string/print_string.h"
+#include "core/version.h"
 
 AAssetManager *FileAccessAndroid::asset_manager = nullptr;
+
+#if defined(PCK_ENCRYPTION_ENABLED)
+
+HashMap<String, String> FileAccessAndroid::directory;
+bool FileAccessAndroid::dir_loaded = false;
+
+void FileAccessAndroid::_load_encrypted_directory() {
+	if (!dir_loaded) {
+		dir_loaded = true;
+		const String &enc_path = "encrypted/directory";
+
+		Ref<FileAccessAndroid> f;
+		f.instantiate();
+		ERR_FAIL_COND_MSG(f.is_null(), "Can't open encrypted file directory.");
+
+		f->asset = AAssetManager_open(asset_manager, enc_path.utf8().get_data(), AASSET_MODE_STREAMING);
+		if (f->asset) {
+			f->path_src = enc_path;
+			f->absolute_path = enc_path;
+			f->len = AAsset_getLength(f->asset);
+			f->pos = 0;
+			f->eof = false;
+
+			uint32_t magic = f->get_32();
+			if (magic == DIR_HEADER_MAGIC) {
+				uint32_t version = f->get_32();
+				uint32_t ver_major = f->get_32();
+				uint32_t ver_minor = f->get_32();
+				f->get_32(); // patch number, not used for validation.
+
+				ERR_FAIL_COND_MSG(version != PACK_FORMAT_VERSION, "Directory version unsupported: " + itos(version) + ".");
+				ERR_FAIL_COND_MSG(ver_major > VERSION_MAJOR || (ver_major == VERSION_MAJOR && ver_minor > VERSION_MINOR), "Directory created with a newer version of the engine: " + itos(ver_major) + "." + itos(ver_minor) + ".");
+
+				uint32_t pack_flags = f->get_32();
+
+				bool enc_directory = (pack_flags & PACK_DIR_ENCRYPTED);
+				ERR_FAIL_COND_MSG(!enc_directory, "Can't open encrypted pack directory.");
+				for (int i = 0; i < 16; i++) {
+					//reserved
+					f->get_32();
+				}
+
+				int file_count = f->get_32();
+
+				Ref<FileAccess> fhead = f;
+				Ref<FileAccessEncrypted> fae;
+				fae.instantiate();
+				ERR_FAIL_COND_MSG(fae.is_null(), "Can't open encrypted directory.");
+
+				Vector<uint8_t> key;
+				key.resize(32);
+				for (int i = 0; i < key.size(); i++) {
+					key.write[i] = script_encryption_key[i];
+				}
+
+				Error err = fae->open_and_parse(f, key, FileAccessEncrypted::MODE_READ, false);
+				ERR_FAIL_COND_MSG(err, "Can't open encrypted directory.");
+				fhead = fae;
+
+				for (int i = 0; i < file_count; i++) {
+					uint32_t sl = fhead->get_32();
+					CharString cs;
+					cs.resize(sl + 1);
+					fhead->get_buffer((uint8_t *)cs.ptr(), sl);
+					cs[sl] = 0;
+
+					String path;
+					path.parse_utf8(cs.ptr());
+
+					sl = fhead->get_32();
+					cs.resize(sl + 1);
+					fhead->get_buffer((uint8_t *)cs.ptr(), sl);
+					cs[sl] = 0;
+
+					String enc_path;
+					enc_path.parse_utf8(cs.ptr());
+
+					directory[path] = enc_path;
+				}
+			}
+		}
+	}
+}
+
+#endif
 
 String FileAccessAndroid::get_path() const {
 	return path_src;
@@ -55,18 +142,65 @@ Error FileAccessAndroid::open_internal(const String &p_path, int p_mode_flags) {
 	}
 
 	ERR_FAIL_COND_V(p_mode_flags & FileAccess::WRITE, ERR_UNAVAILABLE); //can't write on android..
-	asset = AAssetManager_open(asset_manager, path.utf8().get_data(), AASSET_MODE_STREAMING);
-	if (!asset) {
-		return ERR_CANT_OPEN;
+
+#if defined(PCK_ENCRYPTION_ENABLED)
+	_load_encrypted_directory();
+	if (directory.has(path)) {
+		const String &enc_path = "encrypted/" + directory[path];
+
+		Ref<FileAccessAndroid> f;
+		f.instantiate();
+		ERR_FAIL_COND_V_MSG(f.is_null(), ERR_CANT_OPEN, "Can't open encrypted file '" + path + "'.");
+
+		f->asset = AAssetManager_open(asset_manager, enc_path.utf8().get_data(), AASSET_MODE_STREAMING);
+		if (!f->asset) {
+			return ERR_CANT_OPEN;
+		}
+		f->path_src = enc_path;
+		f->absolute_path = enc_path;
+		f->len = AAsset_getLength(f->asset);
+		f->pos = 0;
+		f->eof = false;
+
+		fae.instantiate();
+		ERR_FAIL_COND_V_MSG(fae.is_null(), ERR_CANT_OPEN, "Can't open encrypted file '" + path + "'.");
+
+		Vector<uint8_t> key;
+		key.resize(32);
+		for (int i = 0; i < key.size(); i++) {
+			key.write[i] = script_encryption_key[i];
+		}
+
+		Error err = fae->open_and_parse(f, key, FileAccessEncrypted::MODE_READ, false);
+		if (err != OK) {
+			if (fae.is_valid()) {
+				fae.unref();
+			}
+			ERR_FAIL_V_MSG(err, "Can't open encrypted file '" + path + "'.");
+		}
+	} else {
+		ERR_FAIL_COND_V_MSG(PackedData::file_require_encrypion(path), ERR_CANT_OPEN, "Can't open encrypted pack-referenced file '" + path + "'.");
+#else
+	{
+#endif
+		asset = AAssetManager_open(asset_manager, path.utf8().get_data(), AASSET_MODE_STREAMING);
+		if (!asset) {
+			return ERR_CANT_OPEN;
+		}
+		len = AAsset_getLength(asset);
+		pos = 0;
+		eof = false;
 	}
-	len = AAsset_getLength(asset);
-	pos = 0;
-	eof = false;
 
 	return OK;
 }
 
 void FileAccessAndroid::_close() {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		fae.unref();
+	}
+#endif
 	if (!asset) {
 		return;
 	}
@@ -79,6 +213,13 @@ bool FileAccessAndroid::is_open() const {
 }
 
 void FileAccessAndroid::seek(uint64_t p_position) {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		fae->seek(p_position);
+		return;
+	}
+#endif
+
 	ERR_FAIL_NULL(asset);
 
 	AAsset_seek(asset, p_position, SEEK_SET);
@@ -92,24 +233,56 @@ void FileAccessAndroid::seek(uint64_t p_position) {
 }
 
 void FileAccessAndroid::seek_end(int64_t p_position) {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		fae->seek_end(p_position);
+		return;
+	}
+#endif
+
 	ERR_FAIL_NULL(asset);
+
 	AAsset_seek(asset, p_position, SEEK_END);
 	pos = len + p_position;
 }
 
 uint64_t FileAccessAndroid::get_position() const {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		return fae->get_position();
+	}
+#endif
+
 	return pos;
 }
 
 uint64_t FileAccessAndroid::get_length() const {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		return fae->get_length();
+	}
+#endif
+
 	return len;
 }
 
 bool FileAccessAndroid::eof_reached() const {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		return fae->eof_reached();
+	}
+#endif
+
 	return eof;
 }
 
 uint8_t FileAccessAndroid::get_8() const {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		return fae->get_8();
+	}
+#endif
+
 	if (pos >= len) {
 		eof = true;
 		return 0;
@@ -123,6 +296,12 @@ uint8_t FileAccessAndroid::get_8() const {
 
 uint64_t FileAccessAndroid::get_buffer(uint8_t *p_dst, uint64_t p_length) const {
 	ERR_FAIL_COND_V(!p_dst && p_length > 0, -1);
+
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		return fae->get_buffer(p_dst, p_length);
+	}
+#endif
 
 	int r = AAsset_read(asset, p_dst, p_length);
 
@@ -140,6 +319,12 @@ uint64_t FileAccessAndroid::get_buffer(uint8_t *p_dst, uint64_t p_length) const 
 }
 
 Error FileAccessAndroid::get_error() const {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		return fae->get_error();
+	}
+#endif
+
 	return eof ? ERR_FILE_EOF : OK; // not sure what else it may happen
 }
 
@@ -158,6 +343,13 @@ bool FileAccessAndroid::file_exists(const String &p_path) {
 	} else if (path.begins_with("res://")) {
 		path = path.substr(6, path.length());
 	}
+
+#if defined(PCK_ENCRYPTION_ENABLED)
+	_load_encrypted_directory();
+	if (directory.has(path)) {
+		path = "encrypted/" + directory[path];
+	}
+#endif
 
 	AAsset *at = AAssetManager_open(asset_manager, path.utf8().get_data(), AASSET_MODE_STREAMING);
 

xianofantasia/Go

core/config/project_settings.h
@@ -137,7 +137,7 @@ protected:
 
 	void _convert_to_last_version(int p_from_version);
 
-	bool _load_resource_pack(const String &p_pack, bool p_replace_files = true, int p_offset = 0);
+	bool _load_resource_pack(const String &p_pack, bool p_replace_files = true, int p_offset = 0, bool p_require_encryption = false);
 
 	void _add_property_info_bind(const Dictionary &p_info);
 
@@ -147,6 +147,11 @@ protected:
 
 protected:
 	static void _bind_methods();
+
+#ifndef DISABLE_DEPRECATED
+	bool _load_resource_pack_bind_compat_76161(const String &p_pack, bool p_replace_files, int p_offset);
+	static void _bind_compatibility_methods();
+#endif
 
 public:
 	static const int CONFIG_VERSION = 5;

editor/export/project_export.cpp
@@ -364,13 +364,9 @@ void ProjectExportDialog::_edit_preset(int p_index) {
 	bool enc_pck_mode = current->get_enc_pck();
 	enc_pck->set_pressed(enc_pck_mode);
 
-	enc_directory->set_disabled(!enc_pck_mode);
 	enc_in_filters->set_editable(enc_pck_mode);
 	enc_ex_filters->set_editable(enc_pck_mode);
 	script_key->set_editable(enc_pck_mode);
-
-	bool enc_directory_mode = current->get_enc_directory();
-	enc_directory->set_pressed(enc_directory_mode);
 
 	String key = current->get_script_encryption_key();
 	if (!updating_script_key) {
@@ -559,23 +555,9 @@ void ProjectExportDialog::_enc_pck_changed(bool p_pressed) {
 	ERR_FAIL_COND(current.is_null());
 
 	current->set_enc_pck(p_pressed);
-	enc_directory->set_disabled(!p_pressed);
 	enc_in_filters->set_editable(p_pressed);
 	enc_ex_filters->set_editable(p_pressed);
 	script_key->set_editable(p_pressed);
-
-	_update_current_preset();
-}
-
-void ProjectExportDialog::_enc_directory_changed(bool p_pressed) {
-	if (updating) {
-		return;
-	}
-
-	Ref<EditorExportPreset> current = get_current_preset();
-	ERR_FAIL_COND(current.is_null());
-
-	current->set_enc_directory(p_pressed);
 
 	_update_current_preset();
 }
@@ -1384,13 +1366,8 @@ ProjectExportDialog::ProjectExportDialog() {
 
 	enc_pck = memnew(CheckButton);
 	enc_pck->connect("toggled", callable_mp(this, &ProjectExportDialog::_enc_pck_changed));
-	enc_pck->set_text(TTR("Encrypt Exported PCK"));
+	enc_pck->set_text(TTR("Encrypt Exported PCK or Android Assets"));
 	sec_vb->add_child(enc_pck);
-
-	enc_directory = memnew(CheckButton);
-	enc_directory->connect("toggled", callable_mp(this, &ProjectExportDialog::_enc_directory_changed));
-	enc_directory->set_text(TTR("Encrypt Index (File Names and Info)"));
-	sec_vb->add_child(enc_directory);
 
 	enc_in_filters = memnew(LineEdit);
 	enc_in_filters->connect("text_changed", callable_mp(this, &ProjectExportDialog::_enc_filters_changed));

platform/android/file_access_android.cpp
@@ -30,9 +30,96 @@
 
 #include "file_access_android.h"
 
+#include "core/object/script_language.h"
 #include "core/string/print_string.h"
+#include "core/version.h"
 
 AAssetManager *FileAccessAndroid::asset_manager = nullptr;
+
+#if defined(PCK_ENCRYPTION_ENABLED)
+
+HashMap<String, String> FileAccessAndroid::directory;
+bool FileAccessAndroid::dir_loaded = false;
+
+void FileAccessAndroid::_load_encrypted_directory() {
+	if (!dir_loaded) {
+		dir_loaded = true;
+		const String &enc_path = "encrypted/directory";
+
+		Ref<FileAccessAndroid> f;
+		f.instantiate();
+		ERR_FAIL_COND_MSG(f.is_null(), "Can't open encrypted file directory.");
+
+		f->asset = AAssetManager_open(asset_manager, enc_path.utf8().get_data(), AASSET_MODE_STREAMING);
+		if (f->asset) {
+			f->path_src = enc_path;
+			f->absolute_path = enc_path;
+			f->len = AAsset_getLength(f->asset);
+			f->pos = 0;
+			f->eof = false;
+
+			uint32_t magic = f->get_32();
+			if (magic == DIR_HEADER_MAGIC) {
+				uint32_t version = f->get_32();
+				uint32_t ver_major = f->get_32();
+				uint32_t ver_minor = f->get_32();
+				f->get_32(); // Patch number, not used for validation.
+
+				ERR_FAIL_COND_MSG(version != PACK_FORMAT_VERSION, "Directory version unsupported: " + itos(version) + ".");
+				ERR_FAIL_COND_MSG(ver_major > VERSION_MAJOR || (ver_major == VERSION_MAJOR && ver_minor > VERSION_MINOR), "Directory created with a newer version of the engine: " + itos(ver_major) + "." + itos(ver_minor) + ".");
+
+				uint32_t pack_flags = f->get_32();
+
+				bool enc_directory = (pack_flags & PACK_DIR_ENCRYPTED);
+				ERR_FAIL_COND_MSG(!enc_directory, "Can't open encrypted pack directory.");
+				for (int i = 0; i < 16; i++) {
+					// Reserved.
+					f->get_32();
+				}
+
+				int file_count = f->get_32();
+
+				Ref<FileAccess> fhead = f;
+				Ref<FileAccessEncrypted> fae;
+				fae.instantiate();
+				ERR_FAIL_COND_MSG(fae.is_null(), "Can't open encrypted directory.");
+
+				Vector<uint8_t> key;
+				key.resize(32);
+				for (int i = 0; i < key.size(); i++) {
+					key.write[i] = script_encryption_key[i];
+				}
+
+				Error err = fae->open_and_parse(f, key, FileAccessEncrypted::MODE_READ, false);
+				ERR_FAIL_COND_MSG(err, "Can't open encrypted directory.");
+				fhead = fae;
+
+				for (int i = 0; i < file_count; i++) {
+					uint32_t sl = fhead->get_32();
+					CharString cs;
+					cs.resize(sl + 1);
+					fhead->get_buffer((uint8_t *)cs.ptr(), sl);
+					cs[sl] = 0;
+
+					String path;
+					path.parse_utf8(cs.ptr());
+
+					sl = fhead->get_32();
+					cs.resize(sl + 1);
+					fhead->get_buffer((uint8_t *)cs.ptr(), sl);
+					cs[sl] = 0;
+
+					String enc_path;
+					enc_path.parse_utf8(cs.ptr());
+
+					directory[path] = enc_path;
+				}
+			}
+		}
+	}
+}
+
+#endif
 
 String FileAccessAndroid::get_path() const {
 	return path_src;
@@ -55,18 +142,65 @@ Error FileAccessAndroid::open_internal(const String &p_path, int p_mode_flags) {
 	}
 
 	ERR_FAIL_COND_V(p_mode_flags & FileAccess::WRITE, ERR_UNAVAILABLE); //can't write on android..
-	asset = AAssetManager_open(asset_manager, path.utf8().get_data(), AASSET_MODE_STREAMING);
-	if (!asset) {
-		return ERR_CANT_OPEN;
+
+#if defined(PCK_ENCRYPTION_ENABLED)
+	_load_encrypted_directory();
+	if (directory.has(path)) {
+		const String &enc_path = "encrypted/" + directory[path];
+
+		Ref<FileAccessAndroid> f;
+		f.instantiate();
+		ERR_FAIL_COND_V_MSG(f.is_null(), ERR_CANT_OPEN, "Can't open encrypted file '" + path + "'.");
+
+		f->asset = AAssetManager_open(asset_manager, enc_path.utf8().get_data(), AASSET_MODE_STREAMING);
+		if (!f->asset) {
+			return ERR_CANT_OPEN;
+		}
+		f->path_src = enc_path;
+		f->absolute_path = enc_path;
+		f->len = AAsset_getLength(f->asset);
+		f->pos = 0;
+		f->eof = false;
+
+		fae.instantiate();
+		ERR_FAIL_COND_V_MSG(fae.is_null(), ERR_CANT_OPEN, "Can't open encrypted file '" + path + "'.");
+
+		Vector<uint8_t> key;
+		key.resize(32);
+		for (int i = 0; i < key.size(); i++) {
+			key.write[i] = script_encryption_key[i];
+		}
+
+		Error err = fae->open_and_parse(f, key, FileAccessEncrypted::MODE_READ, false);
+		if (err != OK) {
+			if (fae.is_valid()) {
+				fae.unref();
+			}
+			ERR_FAIL_V_MSG(err, "Can't open encrypted file '" + path + "'.");
+		}
+	} else {
+		ERR_FAIL_COND_V_MSG(PackedData::file_requires_encryption(path), ERR_CANT_OPEN, "Can't open encrypted pack-referenced file '" + path + "'.");
+#else
+	{
+#endif
+		asset = AAssetManager_open(asset_manager, path.utf8().get_data(), AASSET_MODE_STREAMING);
+		if (!asset) {
+			return ERR_CANT_OPEN;
+		}
+		len = AAsset_getLength(asset);
+		pos = 0;
+		eof = false;
 	}
-	len = AAsset_getLength(asset);
-	pos = 0;
-	eof = false;
 
 	return OK;
 }
 
 void FileAccessAndroid::_close() {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		fae.unref();
+	}
+#endif
 	if (!asset) {
 		return;
 	}
@@ -79,6 +213,13 @@ bool FileAccessAndroid::is_open() const {
 }
 
 void FileAccessAndroid::seek(uint64_t p_position) {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		fae->seek(p_position);
+		return;
+	}
+#endif
+
 	ERR_FAIL_NULL(asset);
 
 	AAsset_seek(asset, p_position, SEEK_SET);
@@ -92,24 +233,56 @@ void FileAccessAndroid::seek(uint64_t p_position) {
 }
 
 void FileAccessAndroid::seek_end(int64_t p_position) {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		fae->seek_end(p_position);
+		return;
+	}
+#endif
+
 	ERR_FAIL_NULL(asset);
+
 	AAsset_seek(asset, p_position, SEEK_END);
 	pos = len + p_position;
 }
 
 uint64_t FileAccessAndroid::get_position() const {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		return fae->get_position();
+	}
+#endif
+
 	return pos;
 }
 
 uint64_t FileAccessAndroid::get_length() const {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		return fae->get_length();
+	}
+#endif
+
 	return len;
 }
 
 bool FileAccessAndroid::eof_reached() const {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		return fae->eof_reached();
+	}
+#endif
+
 	return eof;
 }
 
 uint8_t FileAccessAndroid::get_8() const {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		return fae->get_8();
+	}
+#endif
+
 	if (pos >= len) {
 		eof = true;
 		return 0;
@@ -193,6 +366,12 @@ uint64_t FileAccessAndroid::get_64() const {
 uint64_t FileAccessAndroid::get_buffer(uint8_t *p_dst, uint64_t p_length) const {
 	ERR_FAIL_COND_V(!p_dst && p_length > 0, -1);
 
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		return fae->get_buffer(p_dst, p_length);
+	}
+#endif
+
 	int r = AAsset_read(asset, p_dst, p_length);
 
 	if (pos + p_length > len) {
@@ -209,6 +388,12 @@ uint64_t FileAccessAndroid::get_buffer(uint8_t *p_dst, uint64_t p_length) const 
 }
 
 Error FileAccessAndroid::get_error() const {
+#if defined(PCK_ENCRYPTION_ENABLED)
+	if (fae.is_valid()) {
+		return fae->get_error();
+	}
+#endif
+
 	return eof ? ERR_FILE_EOF : OK; // not sure what else it may happen
 }
 
@@ -239,6 +424,13 @@ bool FileAccessAndroid::file_exists(const String &p_path) {
 	} else if (path.begins_with("res://")) {
 		path = path.substr(6, path.length());
 	}
+
+#if defined(PCK_ENCRYPTION_ENABLED)
+	_load_encrypted_directory();
+	if (directory.has(path)) {
+		path = "encrypted/" + directory[path];
+	}
+#endif
 
 	AAsset *at = AAssetManager_open(asset_manager, path.utf8().get_data(), AASSET_MODE_STREAMING);
 