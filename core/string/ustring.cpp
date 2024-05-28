2fce0c7357eb72cd851f8af91157a10558b20fab
A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>
2024-05-28 12:53:09 +0200

    Add `replace_chars` method

--- core/string/ustring.cpp
+++ core/string/ustring.cpp
@@ -4173,6 +4173,35 @@ String String::replace_char(char32_t p_key, char32_t p_with) const {
 	return new_string;
 }
 
+String String::replace_chars(const Vector<char32_t> &p_keys, char32_t p_with) const {
+	ERR_FAIL_COND_V_MSG(p_with == 0, *this, "`with` must not be null.");
+
+	if (p_keys.is_empty() || is_empty()) {
+		return *this;
+	}
+
+	String new_string;
+
+	new_string.resize(size());
+
+	char32_t *new_ptrw = new_string.ptrw();
+	const char32_t *old_ptr = ptr();
+
+	while (*old_ptr) {
+		if (p_keys.has(*old_ptr)) {
+			*new_ptrw = p_with;
+		} else {
+			*new_ptrw = *old_ptr;
+		}
+		++new_ptrw;
+		++old_ptr;
+	}
+
+	*new_ptrw = 0;
+
+	return new_string;
+}
+
 String String::replacen(const String &p_key, const String &p_with) const {
 	return _replace_common(*this, p_key, p_with, true);
 }
