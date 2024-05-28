70eed8ddca1b1c171019c647fbe88e47db09a894
A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>
2024-05-28 14:13:34 +0200

    Add tests for `remove_chars`

--- tests/core/string/test_string.h
+++ tests/core/string/test_string.h
@@ -474,6 +474,13 @@ TEST_CASE("[String] remove_char") {
 	CHECK(s.remove_char('x') == "Banana");
 }
 
+TEST_CASE("[String] remove_chars") {
+	String s = "Banana";
+	CHECK(s.remove_chars({ 'B', 'a' }) == "nn");
+	CHECK(s.remove_chars({}) == "Banana");
+	CHECK(s.remove_chars({ 'x', 'y' }) == "Banana");
+}
+
 TEST_CASE("[String] Number to string") {
 	CHECK(String::num(0) == "0.0"); // The method takes double, so always add zeros.
 	CHECK(String::num(0.0) == "0.0");
