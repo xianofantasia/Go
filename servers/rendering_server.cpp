0a69239d9a544c32a8ebd2f08c9a32b16f5d5bf0
Hugo Locurcio <hugo.locurcio@hugo.pro>
2024-11-07 00:37:26 +0100

    Enable anisotropic filtering for 3D materials by default on desktop
    This results in better appearance for textures viewed from oblique
    angles. Many other engines now enable it by default.
    On today's desktop platforms, the performance impact is minimal if
    other effects are already taxing the GPU.
    
    The default value chosen (8x) is a good balance between performance
    and quality. Depending on the scene, you may be able to use 16x
    anisotropic filtering with little to no cost in terms of frame time.
    
    The anisotropic filtering quality can be configured as usual in the
    Project Settings. Setting it to 1x will disable it even if individual
    materials have it enabled, so this is easy to revert if you don't
    want it.

--- servers/rendering_server.cpp
+++ servers/rendering_server.cpp
@@ -3603,7 +3603,10 @@ void RenderingServer::init() {
 	GLOBAL_DEF_RST("rendering/driver/depth_prepass/disable_for_vendors", "PowerVR,Mali,Adreno,Apple");
 
 	GLOBAL_DEF_RST("rendering/textures/default_filters/use_nearest_mipmap_filter", false);
-	GLOBAL_DEF_RST(PropertyInfo(Variant::INT, "rendering/textures/default_filters/anisotropic_filtering_level", PROPERTY_HINT_ENUM, String::utf8("Disabled (Fastest),2× (Faster),4× (Fast),8× (Average),16× (Slow)")), 2);
+	GLOBAL_DEF_RST(PropertyInfo(Variant::INT, "rendering/textures/default_filters/anisotropic_filtering_level", PROPERTY_HINT_ENUM, String::utf8("Disabled (Fastest),2× (Faster),4× (Fast),8× (Average),16× (Slow)")), 3); // 8x anisotrophic filtering.
+
+	// Disable anisotropic filtering on mobile by default as it's relatively expensive.
+	GLOBAL_DEF_RST("rendering/textures/default_filters/anisotropic_filtering_level.mobile", 0); // Disabled
 
 	GLOBAL_DEF(PropertyInfo(Variant::INT, "rendering/camera/depth_of_field/depth_of_field_bokeh_shape", PROPERTY_HINT_ENUM, "Box (Fast),Hexagon (Average),Circle (Slowest)"), 1);
 	GLOBAL_DEF(PropertyInfo(Variant::INT, "rendering/camera/depth_of_field/depth_of_field_bokeh_quality", PROPERTY_HINT_ENUM, "Very Low (Fastest),Low (Fast),Medium (Average),High (Slow)"), 1);
