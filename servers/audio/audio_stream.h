3018492abe4a988b4c0629cf6f4825f850b513d1
Capry <lunacapryy@gmail.com>
2024-11-21 18:32:47 +0100

    Add metadata tags to WAV and OGG audio streams

--- servers/audio/audio_stream.h
+++ servers/audio/audio_stream.h
@@ -181,6 +181,7 @@ protected:
 	GDVIRTUAL0RC(bool, _has_loop)
 	GDVIRTUAL0RC(int, _get_bar_beats)
 	GDVIRTUAL0RC(int, _get_beat_count)
+	GDVIRTUAL0RC(Dictionary, _get_tags);
 	GDVIRTUAL0RC(TypedArray<Dictionary>, _get_parameter_list)
 
 public:
@@ -191,6 +192,7 @@ public:
 	virtual bool has_loop() const;
 	virtual int get_bar_beats() const;
 	virtual int get_beat_count() const;
+	virtual Dictionary get_tags() const;
 
 	virtual double get_length() const;
 	virtual bool is_monophonic() const;
