93fdcc303fe4dcced6eec80af06e27464143b532
thimenesup <thimenesup@gmail.com>
2024-12-05 18:32:45 +0100

    Implement Buffer Device Address for Rendering Device Vulkan and DirectX12

--- servers/rendering/rendering_device_commons.h
+++ servers/rendering/rendering_device_commons.h
@@ -881,6 +881,7 @@ public:
 		SUPPORTS_ATTACHMENT_VRS,
 		// If not supported, a fragment shader with only side effets (i.e., writes  to buffers, but doesn't output to attachments), may be optimized down to no-op by the GPU driver.
 		SUPPORTS_FRAGMENT_SHADER_WITH_ONLY_SIDE_EFFECTS,
+		SUPPORTS_BUFFER_ADDRESS,
 	};
 
 	enum SubgroupOperations {
