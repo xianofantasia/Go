Reham-Ali/mygodot

SCons: Make builders prettier, utilize `constexpr`

core/extension/make_interface_dumper.py
@@ -1,55 +1,37 @@
-import zlib
+import methods
 
 
 def run(target, source, env):
-    src = str(source[0])
-    dst = str(target[0])
-    with open(src, "rb") as f, open(dst, "w", encoding="utf-8", newline="\n") as g:
-        buf = f.read()
-        decomp_size = len(buf)
+    buffer = methods.get_buffer(source[0])
+    decomp_size = len(buffer)
+    buffer = methods.compress_buffer(buffer)
 
-        # Use maximum zlib compression level to further reduce file size
-        # (at the cost of initial build times).
-        buf = zlib.compress(buf, zlib.Z_BEST_COMPRESSION)
-
-        g.write(
-            """/* THIS FILE IS GENERATED DO NOT EDIT */
-#ifndef GDEXTENSION_INTERFACE_DUMP_H
-#define GDEXTENSION_INTERFACE_DUMP_H
-
+    with methods.generated_wrapper(target[0]) as file:
+        file.write(f"""\
 #ifdef TOOLS_ENABLED
 
 #include "core/io/compression.h"
 #include "core/io/file_access.h"
 #include "core/string/ustring.h"
 
-"""
-        )
+constexpr inline int _gdextension_interface_data_compressed_size = {len(buffer)};
+constexpr inline int _gdextension_interface_data_uncompressed_size = {decomp_size};
+constexpr inline unsigned char _gdextension_interface_data_compressed[] = {{
+	{methods.format_buffer(buffer, 1)}
+}};
 
-        g.write("static const int _gdextension_interface_data_compressed_size = " + str(len(buf)) + ";\n")
-        g.write("static const int _gdextension_interface_data_uncompressed_size = " + str(decomp_size) + ";\n")
-        g.write("static const unsigned char _gdextension_interface_data_compressed[] = {\n")
-        for i in range(len(buf)):
-            g.write("\t" + str(buf[i]) + ",\n")
-        g.write("};\n")
-
-        g.write(
-            """
-class GDExtensionInterfaceDump {
-    public:
-        static void generate_gdextension_interface_file(const String &p_path) {
-            Ref<FileAccess> fa = FileAccess::open(p_path, FileAccess::WRITE);
-            ERR_FAIL_COND_MSG(fa.is_null(), vformat("Cannot open file '%s' for writing.", p_path));
-            Vector<uint8_t> data;
-            data.resize(_gdextension_interface_data_uncompressed_size);
-            int ret = Compression::decompress(data.ptrw(), _gdextension_interface_data_uncompressed_size, _gdextension_interface_data_compressed, _gdextension_interface_data_compressed_size, Compression::MODE_DEFLATE);
-            ERR_FAIL_COND_MSG(ret == -1, "Compressed file is corrupt.");
-            fa->store_buffer(data.ptr(), data.size());
-        };
-};
+class GDExtensionInterfaceDump {{
+	public:
+		static void generate_gdextension_interface_file(const String &p_path) {{
+			Ref<FileAccess> fa = FileAccess::open(p_path, FileAccess::WRITE);
+			ERR_FAIL_COND_MSG(fa.is_null(), vformat("Cannot open file '%s' for writing.", p_path));
+			Vector<uint8_t> data;
+			data.resize(_gdextension_interface_data_uncompressed_size);
+			int ret = Compression::decompress(data.ptrw(), _gdextension_interface_data_uncompressed_size, _gdextension_interface_data_compressed, _gdextension_interface_data_compressed_size, Compression::MODE_DEFLATE);
+			ERR_FAIL_COND_MSG(ret == -1, "Compressed file is corrupt.");
+			fa->store_buffer(data.ptr(), data.size());
+		}};
+}};
 
 #endif // TOOLS_ENABLED
-
-#endif // GDEXTENSION_INTERFACE_DUMP_H
-"""
-        )
+""")

editor/template_builders.py
@@ -1,7 +1,8 @@
 """Functions used to generate source files during build time"""
 
 import os
-from io import StringIO
+
+import methods
 
 
 def parse_template(inherits, source, delimiter):
@@ -36,54 +37,36 @@ def parse_template(inherits, source, delimiter):
             script_template["script"].replace('"', '\\"').lstrip().replace("\n", "\\n").replace("\t", "_TS_")
         )
         return (
-            '{ String("'
-            + script_template["inherits"]
-            + '"), String("'
-            + script_template["name"]
-            + '"),  String("'
-            + script_template["description"]
-            + '"),  String("'
-            + script_template["script"]
-            + '")'
-            + " },\n"
+            f'{{ String("{script_template["inherits"]}"), '
+            + f'String("{script_template["name"]}"), '
+            + f'String("{script_template["description"]}"), '
+            + f'String("{script_template["script"]}") }},'
         )
 
 
 def make_templates(target, source, env):
-    dst = str(target[0])
-    with StringIO() as s:
-        s.write("/* THIS FILE IS GENERATED DO NOT EDIT */\n\n")
-        s.write("#ifndef _CODE_TEMPLATES_H\n")
-        s.write("#define _CODE_TEMPLATES_H\n\n")
-        s.write('#include "core/object/object.h"\n')
-        s.write('#include "core/object/script_language.h"\n')
+    delimiter = "#"  # GDScript single line comment delimiter by default.
+    if source:
+        ext = os.path.splitext(str(source[0]))[1]
+        if ext == ".cs":
+            delimiter = "//"
 
-        delimiter = "#"  # GDScript single line comment delimiter by default.
-        if source:
-            ext = os.path.splitext(str(source[0]))[1]
-            if ext == ".cs":
-                delimiter = "//"
+    parsed_templates = []
 
-        parsed_template_string = ""
-        number_of_templates = 0
+    for filepath in source:
+        filepath = str(filepath)
+        node_name = os.path.basename(os.path.dirname(filepath))
+        parsed_templates.append(parse_template(node_name, filepath, delimiter))
 
-        for filepath in source:
-            filepath = str(filepath)
-            node_name = os.path.basename(os.path.dirname(filepath))
-            parsed_template = parse_template(node_name, filepath, delimiter)
-            parsed_template_string += "\t" + parsed_template
-            number_of_templates += 1
+    parsed_template_string = "\n\t".join(parsed_templates)
 
-        s.write("\nstatic const int TEMPLATES_ARRAY_SIZE = " + str(number_of_templates) + ";\n")
-        s.write(
-            "\nstatic const struct ScriptLanguage::ScriptTemplate TEMPLATES[" + str(number_of_templates) + "] = {\n"
-        )
+    with methods.generated_wrapper(target[0], prefix="code") as file:
+        file.write(f"""\
+#include "core/object/object.h"
+#include "core/object/script_language.h"
 
-        s.write(parsed_template_string)
-
-        s.write("};\n")
-
-        s.write("\n#endif\n")
-
-        with open(dst, "w", encoding="utf-8", newline="\n") as f:
-            f.write(s.getvalue())
+constexpr inline int TEMPLATES_ARRAY_SIZE = {len(parsed_templates)};
+static const struct ScriptLanguage::ScriptTemplate TEMPLATES[TEMPLATES_ARRAY_SIZE] = {{
+	{parsed_template_string}
+}};
+""")

main/main_builders.py
@@ -1,60 +1,42 @@
 """Functions used to generate source files during build time"""
 
+import methods
+
 
 def make_splash(target, source, env):
-    src = str(source[0])
-    dst = str(target[0])
+    buffer = methods.get_buffer(source[0])
 
-    with open(src, "rb") as f:
-        buf = f.read()
-
-    with open(dst, "w", encoding="utf-8", newline="\n") as g:
-        g.write("/* THIS FILE IS GENERATED DO NOT EDIT */\n")
-        g.write("#ifndef BOOT_SPLASH_H\n")
-        g.write("#define BOOT_SPLASH_H\n")
+    with methods.generated_wrapper(target[0]) as file:
         # Use a neutral gray color to better fit various kinds of projects.
-        g.write("static const Color boot_splash_bg_color = Color(0.14, 0.14, 0.14);\n")
-        g.write("static const unsigned char boot_splash_png[] = {\n")
-        for i in range(len(buf)):
-            g.write(str(buf[i]) + ",\n")
-        g.write("};\n")
-        g.write("#endif")
+        file.write(f"""\
+static const Color boot_splash_bg_color = Color(0.14, 0.14, 0.14);
+constexpr inline const unsigned char boot_splash_png[] = {{
+	{methods.format_buffer(buffer, 1)}
+}};
+""")
 
 
 def make_splash_editor(target, source, env):
-    src = str(source[0])
-    dst = str(target[0])
+    buffer = methods.get_buffer(source[0])
 
-    with open(src, "rb") as f:
-        buf = f.read()
-
-    with open(dst, "w", encoding="utf-8", newline="\n") as g:
-        g.write("/* THIS FILE IS GENERATED DO NOT EDIT */\n")
-        g.write("#ifndef BOOT_SPLASH_EDITOR_H\n")
-        g.write("#define BOOT_SPLASH_EDITOR_H\n")
+    with methods.generated_wrapper(target[0]) as file:
         # The editor splash background color is taken from the default editor theme's background color.
         # This helps achieve a visually "smoother" transition between the splash screen and the editor.
-        g.write("static const Color boot_splash_editor_bg_color = Color(0.125, 0.145, 0.192);\n")
-        g.write("static const unsigned char boot_splash_editor_png[] = {\n")
-        for i in range(len(buf)):
-            g.write(str(buf[i]) + ",\n")
-        g.write("};\n")
-        g.write("#endif")
+        file.write(f"""\
+static const Color boot_splash_editor_bg_color = Color(0.125, 0.145, 0.192);
+constexpr inline const unsigned char boot_splash_editor_png[] = {{
+	{methods.format_buffer(buffer, 1)}
+}};
+""")
 
 
 def make_app_icon(target, source, env):
-    src = str(source[0])
-    dst = str(target[0])
+    buffer = methods.get_buffer(source[0])
 
-    with open(src, "rb") as f:
-        buf = f.read()
-
-    with open(dst, "w", encoding="utf-8", newline="\n") as g:
-        g.write("/* THIS FILE IS GENERATED DO NOT EDIT */\n")
-        g.write("#ifndef APP_ICON_H\n")
-        g.write("#define APP_ICON_H\n")
-        g.write("static const unsigned char app_icon_png[] = {\n")
-        for i in range(len(buf)):
-            g.write(str(buf[i]) + ",\n")
-        g.write("};\n")
-        g.write("#endif")
+    with methods.generated_wrapper(target[0]) as file:
+        # Use a neutral gray color to better fit various kinds of projects.
+        file.write(f"""\
+constexpr inline const unsigned char app_icon_png[] = {{
+	{methods.format_buffer(buffer, 1)}
+}};
+""")

scene/theme/icons/default_theme_icons_builders.py
@@ -1,51 +1,36 @@
 """Functions used to generate source files during build time"""
 
 import os
-from io import StringIO
 
-from methods import to_raw_cstring
+import methods
 
 
 # See also `editor/icons/editor_icons_builders.py`.
 def make_default_theme_icons_action(target, source, env):
-    dst = str(target[0])
-    svg_icons = [str(x) for x in source]
+    icons_names = []
+    icons_raw = []
 
-    with StringIO() as icons_string, StringIO() as s:
-        for svg in svg_icons:
-            with open(svg, "r") as svgf:
-                icons_string.write("\t%s,\n" % to_raw_cstring(svgf.read()))
+    for svg in source:
+        path = str(svg)
+        with open(path, encoding="utf-8", newline="\n") as file:
+            icons_raw.append(methods.to_raw_cstring(file.read()))
 
-        s.write("/* THIS FILE IS GENERATED DO NOT EDIT */\n\n")
-        s.write('#include "modules/modules_enabled.gen.h"\n\n')
-        s.write("#ifndef _DEFAULT_THEME_ICONS_H\n")
-        s.write("#define _DEFAULT_THEME_ICONS_H\n")
-        s.write("static const int default_theme_icons_count = {};\n\n".format(len(svg_icons)))
-        s.write("#ifdef MODULE_SVG_ENABLED\n")
-        s.write("static const char *default_theme_icons_sources[] = {\n")
-        s.write(icons_string.getvalue())
-        s.write("};\n")
-        s.write("#endif // MODULE_SVG_ENABLED\n\n")
-        s.write("static const char *default_theme_icons_names[] = {\n")
+        name = os.path.splitext(os.path.basename(path))[0]
+        icons_names.append(f'"{name}"')
 
-        index = 0
-        for f in svg_icons:
-            fname = str(f)
+    icons_names_str = ",\n\t".join(icons_names)
+    icons_raw_str = ",\n\t".join(icons_raw)
 
-            # Trim the `.svg` extension from the string.
-            icon_name = os.path.basename(fname)[:-4]
+    with methods.generated_wrapper(target[0]) as file:
+        file.write(f"""\
+#include "modules/modules_enabled.gen.h"
 
-            s.write('\t"{0}"'.format(icon_name))
+constexpr inline int default_theme_icons_count = {len(icons_names)};
+constexpr inline const char *default_theme_icons_sources[] = {{
+	{icons_raw_str}
+}};
 
-            if fname != svg_icons[-1]:
-                s.write(",")
-            s.write("\n")
-
-            index += 1
-
-        s.write("};\n")
-
-        s.write("#endif\n")
-
-        with open(dst, "w", encoding="utf-8", newline="\n") as f:
-            f.write(s.getvalue())
+constexpr inline const char *default_theme_icons_names[] = {{
+	{icons_names_str}
+}};
+""")

xianofantasia/Go

core/extension/make_interface_dumper.py
@@ -1,55 +1,37 @@
-import zlib
+import methods
 
 
 def run(target, source, env):
-    src = str(source[0])
-    dst = str(target[0])
-    with open(src, "rb") as f, open(dst, "w", encoding="utf-8", newline="\n") as g:
-        buf = f.read()
-        decomp_size = len(buf)
+    buffer = methods.get_buffer(source[0])
+    decomp_size = len(buffer)
+    buffer = methods.compress_buffer(buffer)
 
-        # Use maximum zlib compression level to further reduce file size
-        # (at the cost of initial build times).
-        buf = zlib.compress(buf, zlib.Z_BEST_COMPRESSION)
-
-        g.write(
-            """/* THIS FILE IS GENERATED DO NOT EDIT */
-#ifndef GDEXTENSION_INTERFACE_DUMP_H
-#define GDEXTENSION_INTERFACE_DUMP_H
-
+    with methods.generated_wrapper(target[0]) as file:
+        file.write(f"""\
 #ifdef TOOLS_ENABLED
 
 #include "core/io/compression.h"
 #include "core/io/file_access.h"
 #include "core/string/ustring.h"
 
-"""
-        )
+inline constexpr int _gdextension_interface_data_compressed_size = {len(buffer)};
+inline constexpr int _gdextension_interface_data_uncompressed_size = {decomp_size};
+inline constexpr unsigned char _gdextension_interface_data_compressed[] = {{
+	{methods.format_buffer(buffer, 1)}
+}};
 
-        g.write("static const int _gdextension_interface_data_compressed_size = " + str(len(buf)) + ";\n")
-        g.write("static const int _gdextension_interface_data_uncompressed_size = " + str(decomp_size) + ";\n")
-        g.write("static const unsigned char _gdextension_interface_data_compressed[] = {\n")
-        for i in range(len(buf)):
-            g.write("\t" + str(buf[i]) + ",\n")
-        g.write("};\n")
-
-        g.write(
-            """
-class GDExtensionInterfaceDump {
-    public:
-        static void generate_gdextension_interface_file(const String &p_path) {
-            Ref<FileAccess> fa = FileAccess::open(p_path, FileAccess::WRITE);
-            ERR_FAIL_COND_MSG(fa.is_null(), vformat("Cannot open file '%s' for writing.", p_path));
-            Vector<uint8_t> data;
-            data.resize(_gdextension_interface_data_uncompressed_size);
-            int ret = Compression::decompress(data.ptrw(), _gdextension_interface_data_uncompressed_size, _gdextension_interface_data_compressed, _gdextension_interface_data_compressed_size, Compression::MODE_DEFLATE);
-            ERR_FAIL_COND_MSG(ret == -1, "Compressed file is corrupt.");
-            fa->store_buffer(data.ptr(), data.size());
-        };
-};
+class GDExtensionInterfaceDump {{
+	public:
+		static void generate_gdextension_interface_file(const String &p_path) {{
+			Ref<FileAccess> fa = FileAccess::open(p_path, FileAccess::WRITE);
+			ERR_FAIL_COND_MSG(fa.is_null(), vformat("Cannot open file '%s' for writing.", p_path));
+			Vector<uint8_t> data;
+			data.resize(_gdextension_interface_data_uncompressed_size);
+			int ret = Compression::decompress(data.ptrw(), _gdextension_interface_data_uncompressed_size, _gdextension_interface_data_compressed, _gdextension_interface_data_compressed_size, Compression::MODE_DEFLATE);
+			ERR_FAIL_COND_MSG(ret == -1, "Compressed file is corrupt.");
+			fa->store_buffer(data.ptr(), data.size());
+		}};
+}};
 
 #endif // TOOLS_ENABLED
-
-#endif // GDEXTENSION_INTERFACE_DUMP_H
-"""
-        )
+""")

editor/template_builders.py
@@ -1,7 +1,8 @@
 """Functions used to generate source files during build time"""
 
 import os
-from io import StringIO
+
+import methods
 
 
 def parse_template(inherits, source, delimiter):
@@ -36,54 +37,36 @@ def parse_template(inherits, source, delimiter):
             script_template["script"].replace('"', '\\"').lstrip().replace("\n", "\\n").replace("\t", "_TS_")
         )
         return (
-            '{ String("'
-            + script_template["inherits"]
-            + '"), String("'
-            + script_template["name"]
-            + '"),  String("'
-            + script_template["description"]
-            + '"),  String("'
-            + script_template["script"]
-            + '")'
-            + " },\n"
+            f'{{ String("{script_template["inherits"]}"), '
+            + f'String("{script_template["name"]}"), '
+            + f'String("{script_template["description"]}"), '
+            + f'String("{script_template["script"]}") }},'
         )
 
 
 def make_templates(target, source, env):
-    dst = str(target[0])
-    with StringIO() as s:
-        s.write("/* THIS FILE IS GENERATED DO NOT EDIT */\n\n")
-        s.write("#ifndef _CODE_TEMPLATES_H\n")
-        s.write("#define _CODE_TEMPLATES_H\n\n")
-        s.write('#include "core/object/object.h"\n')
-        s.write('#include "core/object/script_language.h"\n')
+    delimiter = "#"  # GDScript single line comment delimiter by default.
+    if source:
+        ext = os.path.splitext(str(source[0]))[1]
+        if ext == ".cs":
+            delimiter = "//"
 
-        delimiter = "#"  # GDScript single line comment delimiter by default.
-        if source:
-            ext = os.path.splitext(str(source[0]))[1]
-            if ext == ".cs":
-                delimiter = "//"
+    parsed_templates = []
 
-        parsed_template_string = ""
-        number_of_templates = 0
+    for filepath in source:
+        filepath = str(filepath)
+        node_name = os.path.basename(os.path.dirname(filepath))
+        parsed_templates.append(parse_template(node_name, filepath, delimiter))
 
-        for filepath in source:
-            filepath = str(filepath)
-            node_name = os.path.basename(os.path.dirname(filepath))
-            parsed_template = parse_template(node_name, filepath, delimiter)
-            parsed_template_string += "\t" + parsed_template
-            number_of_templates += 1
+    parsed_template_string = "\n\t".join(parsed_templates)
 
-        s.write("\nstatic const int TEMPLATES_ARRAY_SIZE = " + str(number_of_templates) + ";\n")
-        s.write(
-            "\nstatic const struct ScriptLanguage::ScriptTemplate TEMPLATES[" + str(number_of_templates) + "] = {\n"
-        )
+    with methods.generated_wrapper(target[0], prefix="code") as file:
+        file.write(f"""\
+#include "core/object/object.h"
+#include "core/object/script_language.h"
 
-        s.write(parsed_template_string)
-
-        s.write("};\n")
-
-        s.write("\n#endif\n")
-
-        with open(dst, "w", encoding="utf-8", newline="\n") as f:
-            f.write(s.getvalue())
+inline constexpr int TEMPLATES_ARRAY_SIZE = {len(parsed_templates)};
+static const struct ScriptLanguage::ScriptTemplate TEMPLATES[TEMPLATES_ARRAY_SIZE] = {{
+	{parsed_template_string}
+}};
+""")

main/main_builders.py
@@ -1,60 +1,42 @@
 """Functions used to generate source files during build time"""
 
+import methods
+
 
 def make_splash(target, source, env):
-    src = str(source[0])
-    dst = str(target[0])
+    buffer = methods.get_buffer(source[0])
 
-    with open(src, "rb") as f:
-        buf = f.read()
-
-    with open(dst, "w", encoding="utf-8", newline="\n") as g:
-        g.write("/* THIS FILE IS GENERATED DO NOT EDIT */\n")
-        g.write("#ifndef BOOT_SPLASH_H\n")
-        g.write("#define BOOT_SPLASH_H\n")
+    with methods.generated_wrapper(target[0]) as file:
         # Use a neutral gray color to better fit various kinds of projects.
-        g.write("static const Color boot_splash_bg_color = Color(0.14, 0.14, 0.14);\n")
-        g.write("static const unsigned char boot_splash_png[] = {\n")
-        for i in range(len(buf)):
-            g.write(str(buf[i]) + ",\n")
-        g.write("};\n")
-        g.write("#endif")
+        file.write(f"""\
+static const Color boot_splash_bg_color = Color(0.14, 0.14, 0.14);
+inline constexpr const unsigned char boot_splash_png[] = {{
+	{methods.format_buffer(buffer, 1)}
+}};
+""")
 
 
 def make_splash_editor(target, source, env):
-    src = str(source[0])
-    dst = str(target[0])
+    buffer = methods.get_buffer(source[0])
 
-    with open(src, "rb") as f:
-        buf = f.read()
-
-    with open(dst, "w", encoding="utf-8", newline="\n") as g:
-        g.write("/* THIS FILE IS GENERATED DO NOT EDIT */\n")
-        g.write("#ifndef BOOT_SPLASH_EDITOR_H\n")
-        g.write("#define BOOT_SPLASH_EDITOR_H\n")
+    with methods.generated_wrapper(target[0]) as file:
         # The editor splash background color is taken from the default editor theme's background color.
         # This helps achieve a visually "smoother" transition between the splash screen and the editor.
-        g.write("static const Color boot_splash_editor_bg_color = Color(0.125, 0.145, 0.192);\n")
-        g.write("static const unsigned char boot_splash_editor_png[] = {\n")
-        for i in range(len(buf)):
-            g.write(str(buf[i]) + ",\n")
-        g.write("};\n")
-        g.write("#endif")
+        file.write(f"""\
+static const Color boot_splash_editor_bg_color = Color(0.125, 0.145, 0.192);
+inline constexpr const unsigned char boot_splash_editor_png[] = {{
+	{methods.format_buffer(buffer, 1)}
+}};
+""")
 
 
 def make_app_icon(target, source, env):
-    src = str(source[0])
-    dst = str(target[0])
+    buffer = methods.get_buffer(source[0])
 
-    with open(src, "rb") as f:
-        buf = f.read()
-
-    with open(dst, "w", encoding="utf-8", newline="\n") as g:
-        g.write("/* THIS FILE IS GENERATED DO NOT EDIT */\n")
-        g.write("#ifndef APP_ICON_H\n")
-        g.write("#define APP_ICON_H\n")
-        g.write("static const unsigned char app_icon_png[] = {\n")
-        for i in range(len(buf)):
-            g.write(str(buf[i]) + ",\n")
-        g.write("};\n")
-        g.write("#endif")
+    with methods.generated_wrapper(target[0]) as file:
+        # Use a neutral gray color to better fit various kinds of projects.
+        file.write(f"""\
+inline constexpr const unsigned char app_icon_png[] = {{
+	{methods.format_buffer(buffer, 1)}
+}};
+""")

scene/theme/icons/default_theme_icons_builders.py
@@ -1,51 +1,36 @@
 """Functions used to generate source files during build time"""
 
 import os
-from io import StringIO
 
-from methods import to_raw_cstring
+import methods
 
 
 # See also `editor/icons/editor_icons_builders.py`.
 def make_default_theme_icons_action(target, source, env):
-    dst = str(target[0])
-    svg_icons = [str(x) for x in source]
+    icons_names = []
+    icons_raw = []
 
-    with StringIO() as icons_string, StringIO() as s:
-        for svg in svg_icons:
-            with open(svg, "r") as svgf:
-                icons_string.write("\t%s,\n" % to_raw_cstring(svgf.read()))
+    for svg in source:
+        path = str(svg)
+        with open(path, encoding="utf-8", newline="\n") as file:
+            icons_raw.append(methods.to_raw_cstring(file.read()))
 
-        s.write("/* THIS FILE IS GENERATED DO NOT EDIT */\n\n")
-        s.write('#include "modules/modules_enabled.gen.h"\n\n')
-        s.write("#ifndef _DEFAULT_THEME_ICONS_H\n")
-        s.write("#define _DEFAULT_THEME_ICONS_H\n")
-        s.write("static const int default_theme_icons_count = {};\n\n".format(len(svg_icons)))
-        s.write("#ifdef MODULE_SVG_ENABLED\n")
-        s.write("static const char *default_theme_icons_sources[] = {\n")
-        s.write(icons_string.getvalue())
-        s.write("};\n")
-        s.write("#endif // MODULE_SVG_ENABLED\n\n")
-        s.write("static const char *default_theme_icons_names[] = {\n")
+        name = os.path.splitext(os.path.basename(path))[0]
+        icons_names.append(f'"{name}"')
 
-        index = 0
-        for f in svg_icons:
-            fname = str(f)
+    icons_names_str = ",\n\t".join(icons_names)
+    icons_raw_str = ",\n\t".join(icons_raw)
 
-            # Trim the `.svg` extension from the string.
-            icon_name = os.path.basename(fname)[:-4]
+    with methods.generated_wrapper(target[0]) as file:
+        file.write(f"""\
+#include "modules/modules_enabled.gen.h"
 
-            s.write('\t"{0}"'.format(icon_name))
+inline constexpr int default_theme_icons_count = {len(icons_names)};
+inline constexpr const char *default_theme_icons_sources[] = {{
+	{icons_raw_str}
+}};
 
-            if fname != svg_icons[-1]:
-                s.write(",")
-            s.write("\n")
-
-            index += 1
-
-        s.write("};\n")
-
-        s.write("#endif\n")
-
-        with open(dst, "w", encoding="utf-8", newline="\n") as f:
-            f.write(s.getvalue())
+inline constexpr const char *default_theme_icons_names[] = {{
+	{icons_names_str}
+}};
+""")