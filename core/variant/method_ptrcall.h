sheddy1/Synk

Add `RequiredPtr<T>` to mark `Object *` arguments and return values as required

core/variant/method_ptrcall.h
@@ -184,6 +184,38 @@ struct PtrToArg<const T *> {
 	}
 };
 
+// This is for RequiredPtr.
+
+template <class T>
+struct PtrToArg<RequiredPtr<T>> {
+	typedef T *EncodeT;
+
+	_FORCE_INLINE_ static T *convert(const void *p_ptr) {
+		if (p_ptr == nullptr) {
+			// Should we show an error?
+			return RequiredPtr<T>(nullptr);
+		}
+		return RequiredPtr<T>(const_cast<T *>(*reinterpret_cast<T *const *>(p_ptr)));
+	}
+
+	_FORCE_INLINE_ static void encode(RequiredPtr<T> p_var, void *p_ptr) {
+		*((T **)p_ptr) = p_var.ptr();
+	}
+};
+
+template <class T>
+struct PtrToArg<const RequiredPtr<T> &> {
+	typedef T *EncodeT;
+
+	_FORCE_INLINE_ static RequiredPtr<T> convert(const void *p_ptr) {
+		if (p_ptr == nullptr) {
+			// Should we show an error?
+			return RequiredPtr<T>(nullptr);
+		}
+		return RequiredPtr<T>(*reinterpret_cast<T *const *>(p_ptr));
+	}
+};
+
 // This is for ObjectID.
 
 template <>

xianofantasia/Go

core/variant/method_ptrcall.h
@@ -179,6 +179,38 @@ struct PtrToArg<const T *> {
 	}
 };
 
+// This is for RequiredPtr.
+
+template <class T>
+struct PtrToArg<RequiredPtr<T>> {
+	typedef T *EncodeT;
+
+	_FORCE_INLINE_ static T *convert(const void *p_ptr) {
+		if (p_ptr == nullptr) {
+			// Should we show an error?
+			return RequiredPtr<T>(nullptr);
+		}
+		return RequiredPtr<T>(*reinterpret_cast<T *const *>(p_ptr));
+	}
+
+	_FORCE_INLINE_ static void encode(RequiredPtr<T> p_var, void *p_ptr) {
+		*((T **)p_ptr) = p_var._internal_ptr();
+	}
+};
+
+template <class T>
+struct PtrToArg<const RequiredPtr<T> &> {
+	typedef T *EncodeT;
+
+	_FORCE_INLINE_ static RequiredPtr<T> convert(const void *p_ptr) {
+		if (p_ptr == nullptr) {
+			// Should we show an error?
+			return RequiredPtr<T>(nullptr);
+		}
+		return RequiredPtr<T>(*reinterpret_cast<T *const *>(p_ptr));
+	}
+};
+
 // This is for ObjectID.
 
 template <>