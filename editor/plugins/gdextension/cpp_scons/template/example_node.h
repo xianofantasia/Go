Izaaqk/Godot

Allow creating GDExtension plugins from inside the Godot editor

editor/plugins/gdextension/cpp_scons/template/example_node.h
@@ -1,0 +1,22 @@
+#ifndef EXAMPLE_NODE_H
+#define EXAMPLE_NODE_H
+
+#include "__LIBRARY_NAME___defines.h"
+
+#if GDEXTENSION
+#include <godot_cpp/classes/node.hpp>
+#elif GODOT_MODULE
+#include "scene/main/node.h"
+#endif
+
+class ExampleNode : public Node {
+	GDCLASS(ExampleNode, Node);
+
+protected:
+	static void _bind_methods();
+
+public:
+	String hello() const;
+};
+
+#endif // EXAMPLE_NODE_H

editor/plugins/gdextension/cpp_scons/template/tests/test_example_node.h
@@ -1,0 +1,15 @@
+#ifndef TEST_EXAMPLE_NODE_H
+#define TEST_EXAMPLE_NODE_H
+
+#include "../example_node.h"
+#include "tests/test_macros.h"
+
+namespace TestExampleNode {
+TEST_CASE("[ExampleNode] Hello") {
+	ExampleNode test = ExampleNode();
+	const String hello_text = test.hello();
+	REQUIRE(hello_text == "Hello, World! From a module.");
+}
+} // namespace TestExampleNode
+
+#endif // TEST_EXAMPLE_NODE_H

xianofantasia/Go

editor/plugins/gdextension/cpp_scons/template/example_node.h
@@ -1,0 +1,24 @@
+#ifndef EXAMPLE_NODE_H
+#define EXAMPLE_NODE_H
+
+#include "__LIBRARY_NAME___defines.h"
+
+#if GDEXTENSION
+#include <godot_cpp/classes/node.hpp>
+#elif GODOT_MODULE
+#include "scene/main/node.h"
+#endif
+
+class ExampleNode : public Node {
+	GDCLASS(ExampleNode, Node);
+
+protected:
+	static void _bind_methods();
+	void _notification(int p_what);
+
+public:
+	void print_hello() const;
+	String return_hello() const;
+};
+
+#endif // EXAMPLE_NODE_H

editor/plugins/gdextension/cpp_scons/template/tests/test_example_node.h
@@ -1,0 +1,15 @@
+#ifndef TEST_EXAMPLE_NODE_H
+#define TEST_EXAMPLE_NODE_H
+
+#include "../example_node.h"
+#include "tests/test_macros.h"
+
+namespace TestExampleNode {
+TEST_CASE("[ExampleNode] Hello") {
+	ExampleNode test = ExampleNode();
+	const String hello_text = test.return_hello();
+	REQUIRE(hello_text == "Hello, World! From a module.");
+}
+} // namespace TestExampleNode
+
+#endif // TEST_EXAMPLE_NODE_H